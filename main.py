4eab3e5a36d625d9b4bd61dc1393bc7ce6ed42ed
illidandidnothignwrong <95207143+illidandidnothignwrong@users.noreply.github.com>
2022-01-23 16:10:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.font
+import math
 root = Tk()
 # color variables
 number_colors = "black"  # buttons
@@ -14,7 +15,7 @@ font_but = tkinter.font.Font(family="Segoe UI ", size=15)
 root.title("Calculator")
 
 root.config(bg=gray_layout)
-root.geometry("300x350") #initial resolution 
+root.geometry("300x350")
 
 e = Entry(root, width=0,  fg="white", bg=gray_layout,
           font=font_disp)
@@ -39,53 +40,65 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
-    global math
-    math = '+'
-    f_num = int(first_number)
+    global math1
+    math1 = '+'
+    f_num = (first_number)
     e.delete(0, END)
 
 
 def button_equal():
     second_number = e.get()
-    e.delete(0, END)
-    if math == '+':
-        e.insert(0, f_num + int(second_number))
-    if math == '/':
-        e.insert(0, f_num / int(second_number))
-    if math == '*':
-        e.insert(0, f_num * int(second_number))
-    if math == '-':
-        e.insert(0, f_num - int(second_number))
+    if math1 != "":
+        e.delete(0, END)
+        if math1 == '+':
+
+            e.insert(0, float(f_num) + float(second_number))
+        if math1 == '/':
+            e.insert(0, float(f_num) / float(second_number))
+        if math1 == '*':
+            e.insert(0, float(f_num) * float(second_number))
+        if math1 == '-':
+            e.insert(0, float(f_num) - float(second_number))
 
 
 def button_subtract():
 
     first_number = e.get()
     global f_num
-    global math
-    math = '-'
-    f_num = int(first_number)
+    global math1
+    math1 = '-'
+    f_num = (first_number)
     e.delete(0, END)
 
 
 def button_multiply():
     first_number = e.get()
     global f_num
-    global math
-    math = '*'
-    f_num = int(first_number)
+    global math1
+    math1 = '*'
+    f_num = (first_number)
     e.delete(0, END)
 
 
 def button_division():
     first_number = e.get()
     global f_num
-    global math
-    math = '/'
-    f_num = int(first_number)
+    global math1
+    math1 = '/'
+    f_num = (first_number)
     e.delete(0, END)
 
-# define buttons 1 , 9  and their logic
+
+def button_sqr():
+    first_number = e.get()
+    global f_num
+    global math1
+    f_num = (first_number)
+    e.delete(0, END)
+    sqrt123 = math.sqrt(int(f_num))
+    e.insert(0, sqrt123)
+
+# define buttons 1 , 9 to
 
 
 size_numberbuttons = 9
@@ -96,25 +109,25 @@ different_buttonsh = 1
 different_buttonsw = 1
 button_tickness = 0
 button_1 = Button(root, text="1", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(1), highlightthickness=button_tickness, highlightbackground="gray", borderwidth=button_tickness, bg=number_colors, fg=text_color, height=button_height, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('1'), highlightthickness=button_tickness, highlightbackground="gray", borderwidth=button_tickness, bg=number_colors, fg=text_color, height=button_height, width=button_width)
 button_2 = Button(root, text="2", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(2), bg=number_colors, fg=text_color, highlightthickness=button_tickness, borderwidth=button_tickness, height=button_height, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('2'), bg=number_colors, fg=text_color, highlightthickness=button_tickness, borderwidth=button_tickness, height=button_height, width=button_width)
 button_3 = Button(root, text="3", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(3), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('3'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_4 = Button(root, text="4", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(4), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('4'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_5 = Button(root, text="5", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(5), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('5'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_6 = Button(root, text="6", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(6), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('6'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_7 = Button(root, text="7", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(7), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('7'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_8 = Button(root, text="8", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(8), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('8'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_9 = Button(root, text="9", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(9), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('9'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_0 = Button(root, text="0", padx=size_numberbuttonsy,
-                  pady=size_numberbuttons, command=lambda: clicked_numbers(0), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+                  pady=size_numberbuttons, command=lambda: clicked_numbers('0'), bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_add1 = Button(root, text="+", padx=size_numberbuttons,
                      pady=size_numberbuttons, command=button_add, bg=gray_layout1, fg="white", borderwidth=button_tickness, width=button_width, height=button_height)
 button_equal1 = Button(root, text="=", height=different_buttonsh, width=different_buttonsw,
@@ -129,6 +142,8 @@ button_divide = Button(root, text="÷", command=button_division, padx=size_numbe
                        pady=size_numberbuttons, bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 button_multiply = Button(root, text="x", command=button_multiply, padx=size_numberbuttonsy,
                          pady=size_numberbuttons, bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
+button_sqr = Button(root, text="√", command=button_sqr, padx=size_numberbuttonsy,
+                    pady=size_numberbuttons, bg=number_colors, fg=text_color, height=button_height, highlightthickness=button_tickness, borderwidth=button_tickness, width=button_width)
 # put buttons on screen''''''''''''''''''''''''''''''''''''''''''''''
 separatenum = 1
 serparatenum1 = 1
@@ -168,7 +183,8 @@ button_multiply.grid(row=3, column=3,
                      padx=separatenum, pady=serparatenum1, sticky="NSEW")
 button_add1.grid(row=5, column=3,
                  padx=separatenum, pady=serparatenum1, sticky="NSEW")
-#font used on each button var on line 12
+button_sqr.grid(row=2, column=2, sticky="NSEW",
+                padx=separatenum, pady=serparatenum1)
 button_1['font'] = font_but
 button_2['font'] = font_but
 button_3['font'] = font_but
